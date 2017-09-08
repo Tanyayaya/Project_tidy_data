c766b5dedb69334ca2f79a26b2c83b2e796b54cc
Tanyayaya <31395377+Tanyayaya@users.noreply.github.com>
2017-09-08 22:19:49 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,18 @@
-setwd("E:/R/cousera")
 library(data.table)
-train_set<-fread("E:/R/cousera/UCI HAR Dataset/train/X_train.txt")
-train_label<-fread("E:/R/cousera/UCI HAR Dataset/train/y_train.txt",
+train_set<-fread("UCI HAR Dataset/train/X_train.txt")
+train_label<-fread("UCI HAR Dataset/train/y_train.txt",
                    col.names = "label")
-train_subject<-fread("E:/R/cousera/UCI HAR Dataset/train/subject_train.txt",
+train_subject<-fread("UCI HAR Dataset/train/subject_train.txt",
                      col.names = "subject")
 train_data<-cbind(train_subject,train_label,train_set)
-test_set<-fread("E:/R/cousera/UCI HAR Dataset/test/X_test.txt")
-test_label<-fread("E:/R/cousera/UCI HAR Dataset/test/y_test.txt",
+test_set<-fread("UCI HAR Dataset/test/X_test.txt")
+test_label<-fread("UCI HAR Dataset/test/y_test.txt",
                   col.names = "label")
-test_subject<-fread("E:/R/cousera/UCI HAR Dataset/test/subject_test.txt",
+test_subject<-fread("UCI HAR Dataset/test/subject_test.txt",
                     col.names="subject")
 test_data<-cbind(test_subject,test_label,test_set)
 data<-rbind(test_data,train_data)
-feature<-read.table("E:/R/cousera/UCI HAR Dataset/features.txt")
+feature<-read.table("UCI HAR Dataset/features.txt")
 feature<-apply(feature,1,"[",2)
 library(stringr)
 mean_std<-which(grepl("std()|mean()",feature))+2
